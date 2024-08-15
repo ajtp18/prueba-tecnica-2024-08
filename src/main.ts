f9033163eed22a4ad2e64a50822f6ce7610bcd9e
Armando Tinoco <ajtp18@hotmail.com>
2024-08-15 03:40:59 -0500

    configuraciones adicionales para verificar cors

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     origin: 'http://localhost:5173/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    allowedHeaders: 'Content-Type, Authorization, Accept',
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
   });
 
   // Configuración de Swagger
